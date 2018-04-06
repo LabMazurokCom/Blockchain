848c0a41d8cd9ae93e366b54af4d7d6203e1ccc1
Lab.Mazurok.com <37841484+lab-mazurok-com@users.noreply.github.com>
2018-04-06 16:04:18 +0300

    JSON output format
    - Parsed exmo (give a check!)
    - Output in JSON

--- Arbitrage/Indicator/courses.php
+++ Arbitrage/Indicator/courses.php
@@ -1,40 +1,34 @@
-<h1>BTC-USDT</h1>
-<?php 
+<?php
+	// JSON array of (bourse, bid, ask)
 	$options  = array('http' => array('user_agent' => 'curl/7.29.0'));
 	$context  = stream_context_create($options);
-
+	echo '{ "list": [';
 	$x = file_get_contents('https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCUSDT', false, $context); 
 	$y = json_decode($x,true); 
-	echo "binance", "\n", $y[bidPrice], "\n", $y[askPrice], "\n";
-	echo "<br />\n";
+	echo '{"bourse":"binance", "bid":', $y[bidPrice], ', "ask":', $y[askPrice], '},';
 
 	$x = file_get_contents('https://www.bitstamp.net/api/v2/ticker/btcusd', false, $context); 
 	$y = json_decode($x,true); 
-	echo "binance", "\n", $y[bid], "\n", $y[ask], "\n";
-	echo "<br />\n";
+	echo '{"bourse":"bitstamp", "bid":', $y[bid], ', "ask":', $y[ask], '},';
 
 	$x = file_get_contents('https://cex.io/api/ticker/BTC/USD', false, $context); 
 	$y = json_decode($x,true); 
-	echo "cex", "\n", $y[bid], "\n", $y[ask], "\n";
-	echo "<br />\n";
+	echo '{"bourse":"cex", "bid":', $y[bid], ', "ask":', $y[ask], '},';
 
 	$x = file_get_contents('https://api.gdax.com/products/BTC-USD/ticker', false, $context); 
 	$y = json_decode($x,true); 
-	echo "gdax", "\n", $y[bid], "\n", $y[ask], "\n";
-	echo "<br />\n";
-/*
+	echo '{"bourse":"gdax", "bid":', $y[bid], ', "ask":', $y[ask], '},';
+
 	$x = file_get_contents('https://api.hitbtc.com/api/2/public/ticker/BTCUSD', false, $context); 
 	$y = json_decode($x,true); 
-	echo "hitbtc", "\n", $y[bid], "\n", $y[ask], "\n";
-	echo "<br />\n";
-*/
+	echo '{"bourse":"hitbtc", "bid":', $y[bid], ', "ask":', $y[ask], '},';
+
 	$x = file_get_contents('https://api.kucoin.com/v1/open/tick?symbol=BTC-USDT', false, $context); 
 	$y = json_decode($x,true); 
-	echo "kucoin", "\n", $y[data][buy], "\n", $y[data][sell], "\n";
-	echo "<br />\n";
+	echo '{"bourse":"kucoin", "bid":', $y[data][buy], ', "ask":', $y[data][sell], '},';
 
 	$x = file_get_contents('https://api.exmo.com/v1/ticker', false, $context); 
 	$y = json_decode($x,true); 
-	echo "exmo", "\n", $y[BTC_USDT][buy_price], "\n", $y[BTC_USDT][sell_price];
-	echo "<br />\n";
+	echo '{"bourse":"exmo", "bid":', $y[BTC_USDT][buy_price], ', "ask":', $y[BTC_USDT][sell_price], '}';
+	echo ']}';
 ?>
