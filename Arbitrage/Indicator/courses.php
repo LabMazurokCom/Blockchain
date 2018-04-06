0bc27e60a0b000005efb5a0a92a35ffc8f900058
Lab.Mazurok.com <37841484+lab-mazurok-com@users.noreply.github.com>
2018-04-06 13:38:26 +0300

    Парсинг и юзерагент
    - Добавлен парсинг для всех бирж кроме exmo.
    - Добавлено указание на юзер-агента без которого не хотела отдавать данные gdax.

--- Arbitrage/Indicator/courses.php
+++ Arbitrage/Indicator/courses.php
@@ -1,15 +1,40 @@
 <h1>BTC-USDT</h1>
-<h2>binance</h2>
-	<?php echo file_get_contents('https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCUSDT'); ?>
-<h2>bitstamp</h2>
-	<?php echo file_get_contents('https://www.bitstamp.net/api/v2/ticker/btcusd'); ?>
-<h2>cex</h2>
-	<?php echo file_get_contents('https://cex.io/api/ticker/BTC/USD'); ?>
-<h2>gdax</h2>
-	<?php echo file_get_contents('https://api.gdax.com/products/BTC-USD/ticker'); ?>
-<h2>hitbtc</h2>
-	<?php echo file_get_contents('https://api.hitbtc.com/api/2/public/ticker/BTCUSD'); ?>
-<h2>kucoin</h2>
-	<?php echo file_get_contents('https://api.kucoin.com/v1/open/tick?symbol=BTC-USDT'); ?>
-<h2>exmo</h2>
-	<?php echo file_get_contents('https://api.exmo.com/v1/ticker'); ?>
+<?php 
+	$options  = array('http' => array('user_agent' => 'curl/7.29.0'));
+	$context  = stream_context_create($options);
+
+	$x = file_get_contents('https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCUSDT', false, $context); 
+	$y = json_decode($x,true); 
+	echo "binance", "\n", $y[bidPrice], "\n", $y[askPrice], "\n";
+	echo "<br />\n";
+
+	$x = file_get_contents('https://www.bitstamp.net/api/v2/ticker/btcusd', false, $context); 
+	$y = json_decode($x,true); 
+	echo "binance", "\n", $y[bid], "\n", $y[ask], "\n";
+	echo "<br />\n";
+
+	$x = file_get_contents('https://cex.io/api/ticker/BTC/USD', false, $context); 
+	$y = json_decode($x,true); 
+	echo "cex", "\n", $y[bid], "\n", $y[ask], "\n";
+	echo "<br />\n";
+
+	$x = file_get_contents('https://api.gdax.com/products/BTC-USD/ticker', false, $context); 
+	$y = json_decode($x,true); 
+	echo "gdax", "\n", $y[bid], "\n", $y[ask], "\n";
+	echo "<br />\n";
+
+	$x = file_get_contents('https://api.hitbtc.com/api/2/public/ticker/BTCUSD', false, $context); 
+	$y = json_decode($x,true); 
+	echo "hitbtc", "\n", $y[bid], "\n", $y[ask], "\n";
+	echo "<br />\n";
+
+	$x = file_get_contents('https://api.kucoin.com/v1/open/tick?symbol=BTC-USDT', false, $context); 
+	$y = json_decode($x,true); 
+	echo "kucoin", "\n", $y[data][buy], "\n", $y[data][sell], "\n";
+	echo "<br />\n";
+
+	$x = file_get_contents('https://api.exmo.com/v1/ticker', false, $context); 
+	$y = json_decode($x,true); 
+	echo "exmo", "\n", $x, "\n";
+	echo "<br />\n";
+?>
