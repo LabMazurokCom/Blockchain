9742f1c2422988710adbad49db0719e51a50772b
Odessit007 <30698380+Odessit007@users.noreply.github.com>
2018-04-20 18:51:59 +0300

    All basic functionality was implemented and tested

--- Arbitrage/Logging/orders_logger.py
+++ Arbitrage/Logging/orders_logger.py
@@ -1,49 +1,51 @@
+"""
+        Calling examples:
+    python orders_logger.py -h                                      # help
+    python orders_logger.py btc_usd                                 # currency pair is the only required argument
+    python orders_logger -c "my_new_config.json" btc_usd            # pass configuration file
+    python orders_logger -l 10 btc_usd                              # pass number of top orders to be processed
+    python orders_logger.py -c "config.json" -l 10 tc_usd           # pass all available arguments
+"""
+
+
 import argparse
 import aiohttp
 import asyncio
 import async_timeout
 import json
-import os
-import requests
 import time
 from pprint import pprint
 
 
-FETCH_TIMEOUT = 0.5   # number of seconds to wait
+FETCH_TIMEOUT = 2   # number of seconds to wait
 
 
 async def fetch(session, url, name):
     """ GET request via aiohttp, returns JSON """
-    start = time.time()
     try:
+        start = time.time()
         with async_timeout.timeout(FETCH_TIMEOUT):
-            if (name == 'binance'):
-                await asyncio.sleep(1)
             async with session.get(url) as response:
                 response_json = await response.json(content_type=None)
-                #print(name, '{:.4f}'.format(time.time() - start))
+                print(name, '+ {:.4f}'.format(time.time() - start))
                 return name, response_json
     except:
+        print(name, '- -1')
         return name, None
 
 
 async def collect_data(urls, names):
     """ creates aiohttp session and waits till all requests are done """
-    start = time.time()
     async with aiohttp.ClientSession() as session:
         tasks = [fetch(session, url, name) for url, name in zip(urls, names)]
-        ans = await asyncio.gather(*tasks)
-        #print('COLLECTING RESPONSES: {:.4f}'.format(time.time() - start))
-        return ans
+        return await asyncio.gather(*tasks)
 
 
-def collector(conf, urls, names, syms, limit):
-    start = time.time()
-    loop = asyncio.get_event_loop()
-    responses = loop.run_until_complete(collect_data(urls, names))
+def process_responses(responses, conf, syms, limit):
     bids = []
     asks = []
     d = {
+        'amount': 0,
         'amount_points': [],
         'optimal_point': {
             'amount': 0,
@@ -52,68 +54,159 @@ def collector(conf, urls, names, syms, limit):
         'profit': 0,
         'profit_points': [],
         'ticker': [],
-        'orders': [],
-        'trade_cnt': 0,
-        'usd_amount': 0
+        'orders': {'bids': {}, 'asks': {}},
+        'trade_cnt': 0
     }
-    with open("log.txt", "w") as log_file, open("last.txt", "w") as last_file, open('test.txt', 'w') as f:
-        for response in responses:
-            exch = response[0]
-            data = response[1]
-            print(exch, end='\n', file=f)
-            if data is not None:
-                try:
-                    path = conf[exch]["path"]
-                    sym = syms[exch]
-                    current_bids = data
-                    for x in path["bids"]:
-                        if x == "{}":
-                            x = x.format(sym)
-                        current_bids = current_bids[x]
-                    current_asks = data
-                    for x in path["asks"]:
-                        if x == "{}":
-                            x = x.format(sym)
-                        current_asks = current_asks[x]
-
-                    bids = []
-                    asks = []
-                    for i in range(min(limit, len(current_bids))):
-                        bids.append((float(current_bids[i][0]), float(current_bids[i][1]), exch))
-                    for i in range(min(limit, len(current_asks))):
-                        asks.append((float(current_asks[i][0]), float(current_asks[i][1]), exch))
-                    tmp = {'ask': current_asks[0], 'bid': current_bids[0], 'exchange': exch}
-                    d['ticker'].append(tmp)
-                    print('ASKS: ', asks, end='\n', file=f)
-                    print('BIDS: ', bids, end='\n', file=f)
-                    print(file=f)
-                except:
-                    tmp = {'ask': 0, 'bid': 0, 'exchange': exch}
-                    d['ticker'].append(tmp)
-                    print('ASKS: []', end='\n', file=f)
-                    print('BIDS: []', end='\n', file=f)
-                    print(file=f)
-            else:  # Some error occurred while getting data for current exchange
+    for response in responses:
+        exch = response[0]
+        data = response[1]
+        if data is not None:
+            try:
+                path = conf[exch]["path"]
+                sym = syms[exch]
+                # extract orders for current exchange
+                current_bids = data
+                for x in path["bids"]:
+                    if x == "{}":
+                        x = x.format(sym)
+                    current_bids = current_bids[x]
+                current_asks = data
+                for x in path["asks"]:
+                    if x == "{}":
+                        x = x.format(sym)
+                    current_asks = current_asks[x]
+                # add current orders to bids and asks arrays
+                for i in range(min(limit, len(current_bids))):
+                    bids.append([float(current_bids[i][0]), float(current_bids[i][1]), exch])
+                for i in range(min(limit, len(current_asks))):
+                    asks.append([float(current_asks[i][0]), float(current_asks[i][1]), exch])
+                # add top bid and ask price to the ticker
+                tmp = {'ask': current_asks[0][0], 'bid': current_bids[0][0], 'exchange': exch}
+                d['ticker'].append(tmp)
+            except:  # Some error occurred while parsing json response for current exchange
                 tmp = {'ask': 0, 'bid': 0, 'exchange': exch}
                 d['ticker'].append(tmp)
-                print('ASKS: []', end='\n', file=f)
-                print('BIDS: []', end='\n', file=f)
-                print(file=f)
-
+        else:  # Some error occurred while making HTTP request for current exchange
+            tmp = {'ask': 0, 'bid': 0, 'exchange': exch}
+            d['ticker'].append(tmp)
+    if  len(bids) > 0  and  len(asks) > 0:
         bids.sort(key = lambda triple: triple[0], reverse = True)   # bids sorted in descending order by price
         asks.sort(key = lambda triple: triple[0])                   # asks sorted in ascending order by price
-        json.dump(d, last_file, indent=4, sort_keys=True)
-        # print('TOTAL: {:.4f}'.format(time.time() - start))
+    return bids, asks, d
 
 
-"""
-        Calling examples:
-    python orders_logger.py -h                                      # help
-    python orders_logger.py btc_usd                                 # currency pair is the only required argument
-    python orders_logger -c "my_new_config.json" btc_usd            # pass configuration file
-    python orders_logger -l 10 btc_usd                              # pass number of top orders to be processed
-    python orders_logger.py -c "config.json" -l 10 tc_usd           # pass all available arguments       
-"""
+def make_logging_entry(bids, asks, d):
+    bx = 0
+    ax = 0
+    bid_count = len(bids)
+    ask_count = len(asks)
+    profit = 0
+    amount = 0
+    trade_cnt = 0
+    profit_points = []
+    amount_points = []
+    alpha = 0.1
+    optimal_amount = 0
+    optimal_profit = 0
+    num = 0
+    bid_orders = {}
+    ask_orders = {}
+
+    while bx < bid_count and ax < ask_count and bids[bx][0] > asks[ax][0]:
+        # print('cur_bid:', bids[bx][0], bids[bx][1], ' |  cur_ask:', asks[ax][0], asks[ax][1])
+        bid_vol = bids[bx][1]
+        ask_vol = asks[ax][1]
+        if bid_vol == 0:
+            bx += 1
+            continue
+        if ask_vol == 0:
+            ax += 1
+            continue
+
+        m = min(bid_vol, ask_vol)
+        current_profit = (bids[bx][0] - asks[ax][0]) * m
+        profit += current_profit
+        amount += asks[ax][0] * m
+        profit_points.append(profit)
+        amount_points.append(amount)
+        # print('trade:', bids[bx][0] - asks[ax][0], m, ' profit =', current_profit)
+        print('volume:', m, 'bid:', bids[bx][0], 'bid_exc:', bids[bx][2], 'ask:', asks[ax][0], 'ask_exc:', asks[ax][2])
+        bid_exch = bids[bx][2]
+        if bid_exch in bid_orders:
+            bid_orders[bid_exch][0] = min(bid_orders[bid_exch][0], bids[bx][0])
+            bid_orders[bid_exch][1] += m
+        else:
+            bid_orders[bid_exch] = [bids[bx][0], m]
+        ask_exch = asks[ax][2]
+        if ask_exch in ask_orders:
+            ask_orders[ask_exch][0] = max(ask_orders[ask_exch][0], asks[ax][0])
+            ask_orders[ask_exch][1] += m
+        else:
+            ask_orders[ask_exch] = [asks[ax][0], m]
+        bids[bx][1] -= m
+        asks[ax][1] -= m
+        trade_cnt += 1
+
+        num += 1
+        if num == 1:
+            prev_amount = amount
+            prev_profit = profit
+        elif num == 2:
+            first_k = (profit - prev_profit) / (amount - prev_amount)
+            prev_amount = amount
+            prev_profit = profit
+        else:
+            k = (profit - prev_profit) / (amount - prev_amount)
+            if k / first_k >= alpha:
+                optimal_amount = amount
+                optimal_profit = profit
+            prev_amount = amount
+            prev_profit = profit
+    d['profit'] = profit
+    d['amount'] = amount
+    d['trade_cnt'] = trade_cnt
+    d['optimal_point']['amount'] = optimal_amount
+    d['optimal_point']['profit'] = optimal_profit
+    d['amount_points'] = amount_points
+    d['profit_points'] = profit_points
+    d['orders']['bids'] = bid_orders
+    d['orders']['asks'] = ask_orders
+    pprint(d)
+    # form list of orders and add to d
+    # return d
+
+
+def collector(conf, urls, names, syms, limit, log_file, last_file, tech_file):
+    print('#Start', time.time())
+    start = time.time()
+    loop = asyncio.get_event_loop()
+    responses = loop.run_until_complete(collect_data(urls, names))
+    print('#Responses: {:.4f}'.format(time.time() - start))
+    start = time.time()
+    bids, asks, d = process_responses(responses, conf, syms, limit)
+    # print()
+    # pprint(bids)
+    # print()
+    # pprint(asks)
+    print('#Extracted and sorted top bids and top asks, filled the ticker: {:.4f}'.format(time.time() - start))
+    bids = [
+        [110, 1, 'b'], [100, 2, 'a'], [90, 1, 'b'], [80, 3, 'c'], [70, 3, 'a']
+    ]
+    asks = [
+        [75, 1, 'a'], [77, 1, 'x'], [79, 3, 'y'], [81, 5, 'z']
+    ]
+
+    make_logging_entry(bids, asks, d)
+    '''
+    with open("log.txt", "w") as log_file, open("last.txt", "w") as last_file, open('test.txt', 'w') as f:
+        last_file.write('\nBids\n')
+        json.dump(bids, last_file, indent=4, sort_keys=True)
+        last_file.write('\nAsks\n')
+        json.dump(asks, last_file, indent=4, sort_keys=True)
+        last_file.write('\n')
+#        json.dump(d, last_file, indent=4, sort_keys=True)
+    # print('TOTAL: {:.4f}'.format(time.time() - start))
+    '''
 
 
 if __name__ == "__main__":
@@ -131,17 +224,18 @@ if __name__ == "__main__":
                         help="how many top orders should be processed (default: 50)")
     parser.add_argument('symbol',
                         help="currency pair")
-    args = parser.parse_args(['-l', '5', '-c', 'test_config.json', 'btc_usd'])
+    args = parser.parse_args(['-l', '5', '-c', 'orders_config.json', 'btc_usd'])
 
 
-    # Access to argumets' values: args.config, args.limit, args.symbol
+    # Access to arguments' values: args.config, args.limit, args.symbol
     symbol = args.symbol
     limit = args.limit
 
 
     try:
-        with open("log_" + symbol + '.txt', 'a') as log_file, \
-                open("last_" + symbol + '.txt', 'w') as last_file:
+        with open('log_' + symbol + '.txt', 'a') as log_file, \
+                open('last_' + symbol + '.txt', 'w') as last_file, \
+                open('tech_' + symbol + '.txt', 'a+') as tech_file:
             conf = json.load(open(args.config))    # load configuration file
             urls = []
             names = []
@@ -158,17 +252,19 @@ if __name__ == "__main__":
                 print("\tERROR")
                 print("No exchange supports given symbol")
                 exit(1)
+            print(urls)
+            N = 1
             i = 0
             T = 0
-            while i < 1:
+            while i < N:
                 i += 1
                 if i % 10 == 0:
                     print('{} / 50'.format(i))
                 start = time.time()
-                collector(conf, urls, names, syms, limit)
+                collector(conf, urls, names, syms, limit, log_file, last_file, tech_file)
                 T += time.time() - start
                 #print('{:.4f}'.format(time.time() - start))
-            print('{:.4f}'.format(T / 100))
+            print('{:.4f}'.format(T / N))
     except FileNotFoundError as e:
         print("\t ERROR")
         print("No such file", args.config)
