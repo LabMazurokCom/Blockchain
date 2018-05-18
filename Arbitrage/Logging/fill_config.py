ff7a19ac307f5194468d19fe65dc90fac5f2d261
Lena19 <lenanaumova19@gmail.com>
2018-05-18 18:31:33 +0300

    Add files via upload

--- Arbitrage/Logging/fill_config.py
+++ Arbitrage/Logging/fill_config.py
@@ -2,10 +2,10 @@ import csv
 import json
 from pprint import pprint
 
-exchanges = ['binance', 'bitstamp', 'cex', 'exmo', 'gdax', 'kucoin']
-conf = json.load(open('orders_config.json'))    # load configuration file
+exchanges = ['binance', 'bitfinex', 'bitstamp', 'bittrex', 'cex', 'cryptopia', 'exmo', 'gdax', 'kraken', 'kucoin', 'poloniex']
+conf = json.load(open('orders_config_extended.json'))    # load configuration file
 for exch in exchanges:
-    with open(exch + '_symbols.txt') as infile:
+    with open('symbols/' + exch + '_symbols.txt') as infile:
         reader = csv.reader(infile)
         for row in reader:
             exch_pair = row[0]
@@ -13,7 +13,10 @@ for exch in exchanges:
             to_sym = row[2]
             our_pair = from_sym.lower() + '_' + to_sym.lower()
             conf[exch]['converter'][our_pair] = exch_pair
+            our_pair = to_sym.lower() + '_' + from_sym.lower()
+            conf[exch]['converter'][our_pair] = exch_pair
 
-with open('orders_config.json', 'w') as outfile:
+
+with open('orders_config_extended.json', 'w') as outfile:
     json.dump(conf, outfile, indent=4, sort_keys=True)
 print('OK')
