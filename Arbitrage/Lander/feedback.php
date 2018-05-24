04e6d239be27b62decc941714b788075379df6bc
Vulfke <vulfke@gmail.com>
2018-05-24 15:59:52 +0300

    Add files via upload
    feedback works

--- Arbitrage/Lander/feedback.php
+++ Arbitrage/Lander/feedback.php
@@ -2,36 +2,29 @@
 
 
   $email_to = "lab@mazurok.com";
-  $email_subject = "Please, contact me";
-  $email_body = "I wan't to ask you about your work";
-  mail($email_to, $email_subject, $email_body);
-  // var_dump($_POST);
-  if(mail($email_to, $email_subject, $email_body)){
+  $email_subject = $_POST['subject'];
+  try {
+    $email_header = $_POST['email'];
+  } catch (Exception $e) {
+      header('Location:mail-error.html');
+      exit();
+  }
+  if(empty($email_header)) {
+    header('Location:mail-error.html');
+    exit();
+  }
+  try {
+      $email_body = $_POST['message'];
+  } catch (Exception $e) {
+      $email_body = "I have nothing to say";
+  }
+
+
+  if(mail($email_to, $email_subject, $email_body, $email_header)){
       header('Location:mail-success.html');
 
   } else {
-      header('Location:mail-error.html');
+        header('Location:mail-error.html');
   }
-// require_once("./include/fgcontactform.php");
-//
-// $formproc = new FGContactForm();
-//
-// //1. Add your email address here.
-// //You can add more than one recipients.
-// $formproc->AddRecipient('yourname@your-website.com'); //<<---Put your
-//                                                           //email address here
-//
-// //2. For better security. Get a random string from
-// // this link: http://tinyurl.com/randstr
-// // and put it here
-// $formproc->SetFormRandomKey('gkEFthfv6gvGAuL');
-//
-// if(isset($_POST['submitted']))
-// {
-//    if($formproc->ProcessForm())
-//    {
-//         $formproc->RedirectToURL("index.html");
-//    }
-// }
 
  ?>
